8990101c3ca5a3d294f559a742b5d5e3f65356d5
wsameer <dev.wsameer@gmail.com>
2025-12-14 22:31:35 -0500

    adds a dev proxy so /api still works

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -33,4 +33,12 @@ export default defineConfig({
       type: 'module',
     },
   })],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
+  }
 })
