74583180aaf43eec5f9093359ea449610b67a05b
wsameer <dev.wsameer@gmail.com>
2025-12-14 20:47:26 -0500

    sorts typescript shared configs

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,6 +1,36 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { VitePWA } from 'vite-plugin-pwa';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  plugins: [react(), VitePWA({
+    registerType: 'prompt',
+    injectRegister: false,
+
+    pwaAssets: {
+      disabled: false,
+      config: true,
+    },
+
+    manifest: {
+      name: 'web',
+      short_name: 'web',
+      description: 'Frontend of my private finance OS',
+      theme_color: '#ffffff',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
+})
