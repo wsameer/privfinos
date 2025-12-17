c6ee2e3bce8359d9b6ee1079fba5376bed6c5e0c
wsameer <dev.wsameer@gmail.com>
2025-12-17 14:24:19 -0500

    removes ui shared package

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,12 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import appLogo from '/favicon.svg'
-import PWABadge from './PWABadge.tsx'
-import './App.css'
+import reactLogo from "./assets/react.svg";
+import appLogo from "/favicon.svg";
+import PWABadge from "./PWABadge.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
@@ -18,20 +14,9 @@ function App() {
         </a>
       </div>
       <h1>SAM web app</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
       <PWABadge />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
